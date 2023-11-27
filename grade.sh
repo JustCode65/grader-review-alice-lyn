c5f1fb20ab84736bad3b0f0c9996f5d8836445f0
Alice Lai <y6lai@ucsd.edu>
2023-11-27 13:13:16 -0800

    s

--- grade.sh
+++ grade.sh
@@ -9,6 +9,25 @@ git clone $1 student-submission
 echo 'Finished cloning'
 
 
+tests=`find student-submission -name "ListExample*.java"`
+
+
+cp $tests *Test*.java grading-area 
+cp *Test*.java grading-area
+cp ./student-submission/* grading-area
+
+cp -R lib grading-area
+cd grading-area
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+for file in $tests
+do  
+    echo $file
+    output=`java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore *Test*.java`
+    echo $output
+
+done
+
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
